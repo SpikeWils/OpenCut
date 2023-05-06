8d90b319e295e61ae271f08178e17520f0382358
Spike Wilson <113636901+SpikeWils@users.noreply.github.com>
2023-05-06 22:54:32 +0100

    Fixed errors in PC App
    PC app now remains open until manually closed. Time pulled from PC not arduino

--- PC App/PC Application.py
+++ PC App/PC Application.py
@@ -1,18 +1,15 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-import serial
 import datetime
-
-# Set up serial communication with Arduino
-ser = serial.Serial('COM3', 9600) # Replace 'COM3' with your serial port name and 9600 with your baud rate
 
 class ArduinoController(QWidget):
     def __init__(self):
         super().__init__()
-        
+
         self.initUI()
-        
+        self.serial = None
+
     def initUI(self):
         # Set up start, pause and menu buttons
         startButton = QPushButton('Start', self)
@@ -21,20 +18,20 @@ class ArduinoController(QWidget):
         pauseButton.clicked.connect(self.pauseButtonClicked)
         menuButton = QPushButton('Menu', self)
         menuButton.clicked.connect(self.menuButtonClicked)
-        
+
         # Set up text input field
         self.textEdit = QTextEdit(self)
         self.textEdit.setPlaceholderText('Type text to be sent to Arduino')
-        
+
         # Set up text display window
         self.textDisplay = QTextEdit(self)
         self.textDisplay.setReadOnly(True)
-        
+
         # Set up date and time label
-        now = datetime.datetime.now()
-        dateTimeLabel = QLabel(now.strftime('%Y-%m-%d %H:%M:%S'), self)
-        dateTimeLabel.setAlignment(Qt.AlignRight)
-        
+        self.dateTimeLabel = QLabel('', self)
+        self.updateDateTime()
+        self.dateTimeLabel.setAlignment(Qt.AlignRight)
+
         # Set up layout
         vbox = QVBoxLayout()
         vbox.addWidget(startButton)
@@ -42,40 +39,45 @@ class ArduinoController(QWidget):
         vbox.addWidget(menuButton)
         vbox.addWidget(self.textEdit)
         vbox.addWidget(self.textDisplay)
-        vbox.addWidget(dateTimeLabel)
-        
+        vbox.addWidget(self.dateTimeLabel)
+
         self.setLayout(vbox)
         self.setGeometry(300, 300, 300, 250)
         self.setWindowTitle('Arduino Controller')
         self.show()
-        
+
     def startButtonClicked(self):
-        # Send text in the text input field to Arduino
+        # Open serial communication with Arduino and send text in the text input field
+        if not self.serial:
+            self.serial = serial.Serial('COM5', 9600) # Replace 'COM3' with your serial port name and 9600 with your baud rate
         text = self.textEdit.toPlainText()
-        ser.write(text.encode())
-        
+        self.serial.write(text.encode())
+
     def pauseButtonClicked(self):
         # Send 'pause' command to Arduino
-        ser.write(b'pause')
-        
+        if self.serial:
+            self.serial.write(b'pause')
+
     def menuButtonClicked(self):
         # Send 'menu' command to Arduino
-        ser.write(b'menu')
-        
+        if self.serial:
+            self.serial.write(b'menu')
+
     def updateDateTime(self):
         # Update date and time label
         now = datetime.datetime.now()
         self.dateTimeLabel.setText(now.strftime('%Y-%m-%d %H:%M:%S'))
-        
+
     def readSerial(self):
         # Read text sent by Arduino and display it in the text display window
-        if ser.in_waiting > 0:
-            text = ser.readline().decode().strip()
+        if self.serial and self.serial.in_waiting > 0:
+            text = self.serial.readline().decode().strip()
             self.textDisplay.append(text)
-            
+
     def closeEvent(self, event):
         # Close serial communication with Arduino when the GUI is closed
-        ser.close()
+        if self.serial:
+            self.serial.close()
         event.accept()
 
 if __name__ == '__main__':
